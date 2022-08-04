d4e278750799ec0162794b29079d16a9928acfd1
nebu <ja_laissaoui@esi.dz>
2022-08-04 12:26:30 +0100

    Added WSECi with MS and WSAPT with MS heuristics

--- src/pyscheduling_cc/SMSP/riwiTi.py
+++ src/pyscheduling_cc/SMSP/riwiTi.py
@@ -1,3 +1,4 @@
+from math import exp
 import sys
 from dataclasses import dataclass, field
 from random import randint, uniform
@@ -217,6 +218,59 @@ class riwiTi_Solution(SingleMachine.SingleSolution):
         return is_valid
 
 class Heuristics():
+
+    @staticmethod
+    def ACT_WSECi(instance : riwiTi_Instance):
+        startTime = perf_counter()
+        solution = riwiTi_Solution(instance)
+        solution.machine.wiTi_index = []
+        ci = 0
+        wiTi = 0
+        remaining_jobs_list = list(range(instance.n))
+        sumP = sum(instance.P)
+        K = Heuristics_Tuning.ACT(instance)
+        rule = lambda job_id : (float(instance.W[job_id])/float(max(instance.R[job_id] - ci,0) + instance.P[job_id]))*exp(-max(instance.D[job_id]-instance.P[job_id]-ci,0)/(K*sumP))
+        while(len(remaining_jobs_list)>0):
+            remaining_jobs_list.sort(reverse=True,key=rule)
+            taken_job = remaining_jobs_list[0]
+            start_time = max(instance.R[taken_job],ci)
+            ci = start_time + instance.P[taken_job]
+            solution.machine.job_schedule.append(SingleMachine.Job(taken_job,start_time,ci))
+            wiTi += instance.W[taken_job]*max(ci-instance.D[taken_job],0)
+            solution.machine.wiTi_index.append(wiTi)
+            remaining_jobs_list.pop(0)
+        solution.machine.objective=solution.machine.wiTi_index[instance.n-1]
+        solution.fix_objective()
+        return Problem.SolveResult(best_solution=solution,runtime=perf_counter()-startTime,solutions=[solution])
+    
+    @staticmethod
+    def ACT_WSAPT(instance : riwiTi_Instance):
+        startTime = perf_counter()
+        solution = riwiTi_Solution(instance)
+        solution.machine.wiTi_index = []
+        ci = min(instance.R)
+        wiTi = 0
+        remaining_jobs_list = list(range(instance.n))
+        sumP = sum(instance.P)
+        K = Heuristics_Tuning.ACT(instance)
+        rule = lambda job_id : (float(instance.W[job_id])/float(instance.P[job_id]))*exp(-max(instance.D[job_id]-instance.P[job_id]-ci,0)/(K*sumP))
+        while(len(remaining_jobs_list)>0):
+            filtered_remaining_jobs_list = list(filter(lambda job_id : instance.R[job_id]<=ci,remaining_jobs_list))
+            filtered_remaining_jobs_list.sort(reverse=True,key=rule)
+            if(len(filtered_remaining_jobs_list)==0):
+                ci = min([instance.R[job_id] for job_id in remaining_jobs_list])
+                filtered_remaining_jobs_list = list(filter(lambda job_id : instance.R[job_id]<=ci,remaining_jobs_list))
+                filtered_remaining_jobs_list.sort(reverse=True,key=rule)
+            taken_job = remaining_jobs_list[0]
+            start_time = max(instance.R[taken_job],ci)
+            ci = start_time + instance.P[taken_job]
+            solution.machine.job_schedule.append(SingleMachine.Job(taken_job,start_time,ci))
+            wiTi += instance.W[taken_job]*max(ci-instance.D[taken_job],0)
+            solution.machine.wiTi_index.append(wiTi)
+            remaining_jobs_list.pop(0)
+        solution.machine.objective=solution.machine.wiTi_index[instance.n-1]
+        solution.fix_objective()
+        return Problem.SolveResult(best_solution=solution,runtime=perf_counter()-startTime,solutions=[solution])
     
     @classmethod
     def all_methods(cls):
@@ -238,3 +292,10 @@ class Metaheuristics():
         """
         return [getattr(cls, func) for func in dir(cls) if not func.startswith("__") and not func == "all_methods"]
 
+class Heuristics_Tuning():
+
+    @staticmethod
+    def ACT(instance : riwiTi_Instance):
+        Tightness = 1 - sum(instance.D)/(instance.n*sum(instance.P))
+        Range = (max(instance.D)-min(instance.D))/sum(instance.P)
+        return 0.2
