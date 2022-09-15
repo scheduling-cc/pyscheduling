7a292ee5964cfb02db97ddbfb77a50d2c6836fe7
athmani <fm_athmani@esi.dz>
2022-09-15 14:59:53 +0200

    Added CP for flowshop and jobshop: Fm, Jm | | Cmax

--- src/pyscheduling/JS/JmCmax.py
+++ src/pyscheduling/JS/JmCmax.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 from pathlib import Path
 from random import randint
+import sys
 from time import perf_counter
 
 
@@ -8,6 +9,15 @@ import pyscheduling.Problem as RootProblem
 from pyscheduling.Problem import Solver
 import pyscheduling.JS.JobShop as JobShop
 
+try:
+    import docplex
+    from docplex.cp.model import CpoModel
+    from docplex.cp.solver.cpo_callback import CpoCallback
+    from docplex.cp.expression import INTERVAL_MAX
+except ImportError:
+    pass
+
+DOCPLEX_IMPORTED = True if "docplex" in sys.modules else False
 
 @dataclass
 class JmCmax_Instance(JobShop.JobShopInstance):
@@ -92,6 +102,172 @@ class JmCmax_Instance(JobShop.JobShopInstance):
         """
         return RootProblem.Objective.Cmax
 
+class ExactSolvers():
+
+    if DOCPLEX_IMPORTED:
+        @staticmethod
+        def csp(instance, **kwargs):
+            return CSP.solve(instance, **kwargs)
+    else:
+        @staticmethod
+        def csp(instance, **kwargs):
+            print("Docplex import error: you can not use this solver")
+            return None
+
+if DOCPLEX_IMPORTED:
+    class CSP():
+
+        CPO_STATUS = {
+            "Feasible": RootProblem.SolveStatus.FEASIBLE,
+            "Optimal": RootProblem.SolveStatus.OPTIMAL
+        }
+
+        class MyCallback(CpoCallback):
+            """A callback used to log the value of cmax at different timestamps
+
+            Args:
+                CpoCallback (_type_): Inherits from CpoCallback
+            """
+
+            def __init__(self, stop_times=[300, 600, 3600, 7200]):
+                self.stop_times = stop_times
+                self.best_values = dict()
+                self.stop_idx = 0
+                self.best_sol_time = 0
+                self.nb_sol = 0
+
+            def invoke(self, solver, event, jsol):
+
+                if event == "Solution":
+                    self.nb_sol += 1
+                    solve_time = jsol.get_info('SolveTime')
+                    self.best_sol_time = solve_time
+
+                    # Go to the next stop time
+                    while self.stop_idx < len(self.stop_times) and solve_time > self.stop_times[self.stop_idx]:
+                        self.stop_idx += 1
+
+                    if self.stop_idx < len(self.stop_times):
+                        # Get important elements
+                        obj_val = jsol.get_objective_values()[0]
+                        self.best_values[self.stop_times[self.stop_idx]] = obj_val
+
+        @staticmethod
+        def _transform_csp_solution(msol, M_k, types_k, instance):
+            """Transforms cp optimizer interval variable into a solution 
+
+            Args:
+                msol (): CPO solution
+                M_k (list[list[interval_var]]): Interval variables represening jobs inside machines k
+                types_k (list[int]): List of job ids inside each machine k
+                instance (RmSijkCmax_Instance): instance corresponding to the solution
+
+            Returns:
+                JobShopSolution: cpoptimizer's solution
+            """
+            sol = JobShop.JobShopSolution(instance)
+            for k in range(instance.m):
+                k_tasks = []
+                for i, oper_k_i in enumerate(M_k[k]):
+                    start, end, _ = msol[oper_k_i]
+                    k_tasks.append(JobShop.Job(types_k[k][i],start,end))
+
+                    k_tasks = sorted(k_tasks, key= lambda x: x[1])
+                    sol.machines[k].job_schedule = k_tasks
+                
+            return sol
+
+        @staticmethod
+        def solve(instance, **kwargs):
+            """ Returns the solution using the Cplex - CP optimizer solver
+
+            Args:
+                instance (Instance): Instance object to solve
+                log_path (str, optional): Path to the log file to output cp optimizer log. Defaults to None to disable logging.
+                time_limit (int, optional): Time limit for executing the solver. Defaults to 300s.
+                threads (int, optional): Number of threads to set for cp optimizer solver. Defaults to 1.
+
+            Returns:
+                SolveResult: The object represeting the solving process result
+            """
+            if "docplex" in sys.modules:
+                
+                # Extracting parameters
+                log_path = kwargs.get("log_path", None)
+                time_limit = kwargs.get("time_limit", 300)
+                nb_threads = kwargs.get("threads", 1)
+                stop_times = kwargs.get(
+                    "stop_times", [time_limit // 4, time_limit // 2, (time_limit * 3) // 4, time_limit])
+
+                # Preparing ranges
+                E = range(instance.n)
+                M = range(instance.m)
+
+                model = CpoModel("JS_Model")
+
+                E_i = [[] for i in E]
+                M_k = [[] for k in M]
+
+                types_k = [ [] for k in M ]
+                for i in E:
+                    for oper in instance.P[i]:
+                        k, p_ik = oper
+                        start_period = (0, INTERVAL_MAX)
+                        oper_i_k = model.interval_var( start = start_period,
+                                                    size = p_ik, optional= False, name=f'E[{i},{k}]')
+                        E_i[i].append(oper_i_k)
+                        
+                        M_k[k].append(oper_i_k)
+                        types_k[k].append(i)
+
+                # No overlap inside machines
+                seq_array = []
+                for k in M:
+                    seq_k = model.sequence_var(M_k[k], types_k[k], name=f"Seq_{k}")
+                    model.add( model.no_overlap(seq_k) )        
+                    seq_array.append(seq_k)
+
+                # Precedence constraint between machines for each job
+                for i in E:
+                    for k in range(1, len(E_i[i])):
+                        model.add( model.end_before_start(E_i[i][k - 1], E_i[i][k]) )
+
+                # Add objective
+                model.add( model.minimize( model.max(model.end_of(job_i) for i in E for job_i in E_i[i]) ) )
+
+                # Adding callback to log stats
+                mycallback = CSP.MyCallback(stop_times=stop_times)
+                model.add_solver_callback(mycallback)
+
+                msol = model.solve(LogVerbosity="Normal", Workers=nb_threads, TimeLimit=time_limit, LogPeriod=1000000,
+                                trace_log=False,  add_log_to_solution=True, RelativeOptimalityTolerance=0)
+
+                if log_path:
+                    logFile = open(log_path, "w")
+                    logFile.write('\n\t'.join(msol.get_solver_log().split("!")))
+
+                sol = CSP._transform_csp_solution(msol, M_k, types_k, instance)
+
+                # Construct the solve result
+                kpis = {
+                    "ObjValue": msol.get_objective_value(),
+                    "ObjBound": msol.get_objective_bounds()[0],
+                    "MemUsage": msol.get_infos()["MemoryUsage"]
+                }
+                
+                solve_result = RootProblem.SolveResult(
+                    best_solution=sol,
+                    runtime=msol.get_infos()["TotalTime"],
+                    time_to_best= mycallback.best_sol_time,
+                    status=CSP.CPO_STATUS.get(
+                        msol.get_solve_status(), RootProblem.SolveStatus.INFEASIBLE),
+                    kpis=kpis
+                )
+
+                return solve_result
+
+            else:
+                print("Docplex import error: you can not use this solver")
 
 class Heuristics():
 
