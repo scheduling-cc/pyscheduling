53753c8c2278c69a30be4bf4099c55df556ee4af
nebu <ja_laissaoui@esi.dz>
2022-09-30 13:11:26 +0100

    Copied the content of the html folder to the root folder

--- src/pyscheduling/PMSP/__init__.py
+++
@@ -1,5 +0,0 @@
-# read version from installed package
-from importlib.metadata import version
-__version__ = version("pyscheduling")
-
-__all__ = ["ParallelMachines", "PM_Methods", "RmSijkCmax", "RmriSijkCmax"]
