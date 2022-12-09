3e60cdb3eda201510ef81ca01fafd69acdfd2123
athmani <fm_athmani@esi.dz>
2022-12-09 15:54:28 +0100

    Added grasp heuristic to flowshop

--- src/pyscheduling/FS/FS_Methods.py
+++ src/pyscheduling/FS/FS_Methods.py
@@ -76,6 +76,54 @@ class Heuristics():
         
         return RootProblem.SolveResult(best_solution=solution, runtime=perf_counter()-start_time, solutions=[solution])
 
+    @staticmethod
+    def grasp(instance: FlowShopInstance, p: float, r: int, n_iterations: int):
+        """Returns the solution using the Greedy randomized adaptive search procedure algorithm
+
+        Args:
+            instance (SingleInstance): The instance to be solved by the heuristic
+            p (float): probability of taking the greedy best solution
+            r (int): percentage of moves to consider to select the best move
+            nb_exec (int): Number of execution of the heuristic
+
+        Returns:
+            Problem.SolveResult: the solver result of the execution of the heuristic
+        """
+        startTime = perf_counter()
+        solveResult = RootProblem.SolveResult()
+        best_solution = None
+        for _ in range(n_iterations):
+            solution = FlowShopSolution(instance)
+            remaining_jobs_list = [i for i in range(instance.n)]
+            while len(remaining_jobs_list) != 0:
+                insertions_list = []
+                for i in remaining_jobs_list:
+                    start_time, end_time = solution.simulate_insert_last(i)
+                    new_obj = solution.simulate_insert_objective(i, start_time, end_time)
+                    insertions_list.append((i, new_obj))
+
+                insertions_list.sort(key=lambda insertion: insertion[1])
+                proba = random.random()
+                if proba < p:
+                    rand_insertion = insertions_list[0]
+                else:
+                    rand_insertion = random.choice(
+                        insertions_list[0:int(instance.n * r)])
+                
+                taken_job, new_obj = rand_insertion
+                solution.job_schedule.append(Job(taken_job, 0, 0))
+                solution.compute_objective(startIndex=len(solution.job_schedule) - 1)
+                remaining_jobs_list.remove(taken_job)
+
+            solveResult.all_solutions.append(solution)
+            if not best_solution or best_solution.objective_value > solution.objective_value:
+                best_solution = solution
+
+        solveResult.best_solution = best_solution
+        solveResult.runtime = perf_counter() - startTime
+        solveResult.solve_status = RootProblem.SolveStatus.FEASIBLE
+        return solveResult
+
     def MINIT(instance : FlowShopInstance):
         """Gupta's MINIT heuristic which is based on iteratively scheduling a new job at the end
         so that it minimizes the idle time at the last machine
