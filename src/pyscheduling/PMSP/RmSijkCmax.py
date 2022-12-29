16e00c37505f7fab0f9e2ad9ca50618169bdfe3a
Mourad Terzi <terziimourad@gmail.com>
2022-12-29 21:11:19 +0100

    feat: generalize the GA for all parallel problems

--- src/pyscheduling/PMSP/RmSijkCmax.py
+++ src/pyscheduling/PMSP/RmSijkCmax.py
@@ -452,17 +452,18 @@ class AntColony(object):
     def __init__(self, instance: RmSijkCmax_Instance, n_ants: int = 60, n_best: int = 1,
                  n_iterations: int = 100, alpha=1, beta=1, phi: float = 0.081, evaporation: float = 0.01,
                  q0: float = 0.5, best_ants: int = 10, pheromone_init=10):
-        """
-        Args:
-            distances (2D numpy.array): Square matrix of distances. Diagonal is assumed to be np.inf.
-            n_ants (int): Number of ants running per iteration
-            n_best (int): Number of best ants who deposit pheromone
-            n_iteration (int): Number of iterations
-            decay (float): Rate it which pheromone decays. The pheromone value is multiplied by decay, so 0.95 will lead to decay, 0.5 to much faster decay.
-            alpha (int or float): exponenet on pheromone, higher alpha gives pheromone more weight. Default=1
-            beta (int or float): exponent on distance, higher beta give distance more weight. Default=1
-        Example:
-            ant_colony = AntColony(german_distances, 100, 20, 2000, 0.95, alpha=1, beta=2)          
+        
+        """_summary_
+            Args:
+                distances (2D numpy.array): Square matrix of distances. Diagonal is assumed to be np.inf.
+                n_ants (int): Number of ants running per iteration
+                n_best (int): Number of best ants who deposit pheromone
+                n_iteration (int): Number of iterations
+                decay (float): Rate it which pheromone decays. The pheromone value is multiplied by decay, so 0.95 will lead to decay, 0.5 to much faster decay.
+                alpha (int or float): exponenet on pheromone, higher alpha gives pheromone more weight. Default=1
+                beta (int or float): exponent on distance, higher beta give distance more weight. Default=1
+            Example:
+                ant_colony = AntColony(german_distances, 100, 20, 2000, 0.95, alpha=1, beta=2)  
         """
         self.instance = instance
         self.n_ants = n_ants
