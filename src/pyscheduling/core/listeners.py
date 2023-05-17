4fbec63a2b0966c5a1e57aa634b2115006a3a5ea
AminosDz <fm_athmani@esi.dz>
2023-05-17 17:17:11 +0200

    Added PlotListener to generic listeners

--- src/pyscheduling/core/listeners.py
+++ src/pyscheduling/core/listeners.py
@@ -5,6 +5,10 @@ from pathlib import Path
 from time import perf_counter
 from typing import TextIO
 
+from plotly.offline import init_notebook_mode
+
+import plotly.graph_objects as go
+
 from pyscheduling.Problem import SolveResult, SolveStatus
 
 
@@ -69,7 +73,7 @@ class FileListener(BaseListener):
     def on_complete(self):
         super().on_complete()
 
-        self._file.write("\n" + '-' * (55 + 5) + "\n\n")
+        self._file.write("\n" + '-' * (80) + "\n\n")
         
         self._file.write(f"{'Search completed':<20} : {self.solve_result.nb_solutions} solution(s) found \n"+
                          f"{'Status code':<20} : {self.solve_result.solve_status}\n\n")
@@ -158,3 +162,37 @@ class CSVListener(BaseListener):
 
         if self._nb_sol % 10 == 0:
             self._file.flush()
+
+@dataclass
+class PlotListener(BaseListener):
+
+    def on_start(self, solve_result):
+        super().on_start(solve_result)
+        self.time_axis = []
+        self.y_axis = []
+        
+        init_notebook_mode(connected = True)
+        self.fig = go.Figure()
+        
+        # Edit the layout
+        self.fig.update_layout(title='Objective value VS Time (s)',
+                        xaxis_title='Time (s)',
+                        yaxis_title='Objective value')
+        
+    def on_complete(self):
+        super().on_complete()
+        
+        self.fig.add_trace(go.Scatter(x=self.time_axis, y=self.y_axis, name='Objective value',
+                         line=dict(color='firebrick', width=4)))
+
+        self.fig.show()
+
+    def on_solution_found(self, new_solution):
+        self._nb_sol += 1
+        time_found = perf_counter() - self._start_time
+        # Check if the new solution is the best one
+        found_new_best = self.check_best_sol(new_solution, time_found)
+        
+        self.time_axis.append(time_found)
+        self.y_axis.append(new_solution.objective_value)
+
