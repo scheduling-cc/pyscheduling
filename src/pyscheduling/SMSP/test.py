52e77bea90df1f66cd744b1d540fdd7640b3eda1
nebu <ja_laissaoui@esi.dz>
2023-07-22 10:20:25 +0100

    removed test files

--- src/pyscheduling/SMSP/test.py
+++
@@ -1,5 +0,0 @@
-import pyscheduling.SMSP.risijwiTi as sm
-instance = sm.risijwiTi_Instance.generate_random(50,Wmax=10,Pmin=1, Pmax=50,alpha=100)
-instance.to_txt("deleteMe.txt")
-solution = sm.Heuristics.ACTS_WSECi(instance)
-print(solution)
